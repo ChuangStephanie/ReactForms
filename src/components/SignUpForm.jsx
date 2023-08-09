2a7ffa65723ed85a05e9f454500473876326f9b8
ChuangStephanie <stephanie231951@yahoo.com>
2023-08-08 20:23:28 -0500

    created sign up form

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,17 +1,53 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function SignUpForm() {
-    const [username, setUsername] = useState(""); 
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null)
-    return (
-        <>
-            <h2>Sign Up!</h2>
-            <form>
-                <label>Username:<input  /></label>
-                <label>Password:<input type="text"/></label>
-                <button type="submit">Submit</button>
-            </form>
-        </>
-    )
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(e) {
+    // prevent browser from reloading page
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({username, password}),
+        }
+      );
+      const result = await response.json();
+      console.log("result", result);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <>
+      <h2>Sign Up!</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </>
+  );
 }
