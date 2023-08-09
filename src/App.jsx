fd88764a0d5109776f1d0e376d6bd97f895881bb
ChuangStephanie <stephanie231951@yahoo.com>
2023-08-08 20:33:42 -0500

    add token and setToken variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import './App.css'
-import SignUpForm from './components/SignUpForm'
-import Authenticate from './components/Authenticate'
-import { useState } from 'react'
+import "./App.css";
+import SignUpForm from "./components/SignUpForm";
+import Authenticate from "./components/Authenticate";
+import { useState } from "react";
 
 function App() {
   const [token, setToken] = useState(null);
 
   return (
     <>
-      <SignUpForm/>
-      <Authenticate/>
+      <Authenticate token={token} setToken={setToken} />
+      <SignUpForm token={token} setToken={setToken} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
