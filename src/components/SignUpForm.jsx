5c676846a3a8ba49b776700df92ec78aa2c8b9f8
ChuangStephanie <stephanie231951@yahoo.com>
2023-08-08 19:47:51 -0500

    added form outline

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -4,5 +4,14 @@ export default function SignUpForm() {
     const [username, setUsername] = useState(""); 
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null)
-    return <h2>Sign Up!</h2>
+    return (
+        <>
+            <h2>Sign Up!</h2>
+            <form>
+                <label>Username:<input  /></label>
+                <label>Password:<input type="text"/></label>
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    )
 }
