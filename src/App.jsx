13fa18ddaf17e28706050b2e10656b663a83d830
ChuangStephanie <stephanie231951@yahoo.com>
2023-08-08 19:38:06 -0500

    added signupform and authenticate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import SignUpForm from './components/SignUpForm'
+import Authenticate from './components/Authenticate'
 
 function App() {
 
   return (
     <>
-      
+      <SignUpForm/>
+      <Authenticate/>
     </>
   )
 }
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react"
+
+export default function SignUpForm() {
+    const [username, setUsername] = useState(""); 
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null)
+    return <h2>Sign Up!</h2>
+}
