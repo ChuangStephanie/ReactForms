94f955766e4e3848afcbb483407daa1db1bbd659
lowl9u <133032114+lowl9u@users.noreply.github.com>
2023-08-09 20:07:17 -0500

    Update Authenticate.jsx

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,30 +1,35 @@
+import React, { useState } from 'react';
+
 export default function Authenticate({ token }) {
-    const [successMessage, setSuccessMessage] = useState(null);
-    const [error, setError] = useState(null);
-  
-    async function handleClick() {
-      try {
-        const response = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        });
-  
-        const result = await response.json();
-        setSuccessMessage(result.message);
-      } catch (error) {
-        setError(error.message);
-      }
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+  const [userData, setUserData] = useState(null); // Add a state for user data
+
+  async function handleClick() {
+    try {
+      const response = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const result = await response.json();
+      setSuccessMessage(result.message);
+      setUserData(result.data); // Set the user data
+    } catch (error) {
+      setError(error.message);
     }
-  
-    return (
-      <div>
-        <h2>Authenticate</h2>
-        {successMessage && <p>{successMessage}</p>}
-        {error && <p>{error}</p>}
-        <button onClick={handleClick}>Authenticate!</button>
-      </div>
-    );
   }
+
+  return (
+    <div>
+      <h2>Authenticate</h2>
+      {successMessage && <p>{successMessage}</p>}
+      {error && <p>{error}</p>}
+      {userData && <p>Welcome, {userData.username}!</p>} {/* Display the username */}
+      <button onClick={handleClick}>Authenticate Token!</button>
+    </div>
+  );
+} 
